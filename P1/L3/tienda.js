e5033d65db86c08c29d234ee799c4e3f21a6a613
lpmachado2020 <lp.machado.2020@alumnos.urjc.es>
2024-02-20 16:18:20 +0100

    Añadida estructura básica de los ficheros

--- P1/L3/tienda.js
+++ P1/L3/tienda.js
@@ -11,17 +11,57 @@
 
 const http = require('http');
 
-//-- El servidor debe escuchar en el puerto 9090
 //-- Definir el puerto a utilizar
 const PUERTO = 9090;
 
-//-- Crear el servidor. La función de retrollamada de
-//-- atención a las peticiones se define detnro de los
-//-- argumentos
+//-- Crear el servidor
 const server = http.createServer((req, res) => {
     
   //-- Indicamos que se ha recibido una petición
   console.log("Petición recibida!");
+
+  //-- Cabecera que indica el tipo de datos del cuerpo de la respuesta: HTML
+  res.setHeader('Content-Type', 'text/html');
+
+  //-- Mensaje del cuerpo
+  res.write(`
+  <!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Enlaces a tus scripts de JavaScript aquí -->
+</head>
+<body>
+
+    <header>
+        <h1>Tienda</h1>
+        <!-- Barra de navegación -->
+        <nav>
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Productos</a></li>
+                <li><a href="#">Carrito</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <!-- Contenido principal de tu tienda, como productos, categorías, etc. -->
+    </main>
+
+    <footer>
+        <!-- Pie de página con información adicional y enlaces útiles -->
+        <p>&copy; 2024 Tienda</p>
+    </footer>
+</body>
+</html>
+`);
+
+  //-- Terminar la respuesta y enviarla
+  res.end();
+
 });
 
 //-- Activar el servidor: ¡Que empiece la fiesta!
