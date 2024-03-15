18fa3829330aaba3eaef9436d8223bac3b38f5c3
lpmachado2020 <lp.machado.2020@alumnos.urjc.es>
2024-03-15 19:38:51 +0100

    Condiciones para servir página main o página error

--- P1/tienda.js
+++ P1/tienda.js
@@ -27,34 +27,44 @@ const server = http.createServer((req, res) => {
     //-- Indicamos que se ha recibido una petición
     console.log("Petición recibida!");
 
-    //-- Valores de la respuesta por defecto
-    let code = 200;
-    let code_msg = "OK";
-    let page = pagina_main;
-
     //-- Analizar el recurso
     //-- Construir el objeto url con la url de la solicitud
     const url = new URL(req.url, 'http://' + req.headers['host']);
     console.log(url.pathname);
 
-    //-- Cualquier recurso que no sea la página principal
-    //-- genera un error
-    if (url.pathname != '/') {
+    //-- Si el recurso es '/' sirve la página principal
+    if (url.pathname == '/') {
+        //-- Valores de la respuesta por defecto
+        code = 200;
+        code_msg = "OK";
+        page = pagina_main;
+
+        //-- Generar la respusta en función de las variables
+        //-- code, code_msg y page
+        res.statusCode = code;
+        res.statusMessage = code_msg;
+        res.setHeader('Content-Type','text/html');
+        res.write(page);
+        res.end();
+    }
+
+    //-- Cualquier recurso que no sea la página principal genera un error
+    else if (url.pathname != '/') {
         code = 404;
         code_msg = "Not Found";
         page = pagina_error;
-    }
 
-    //-- Generar la respusta en función de las variables
-    //-- code, code_msg y page
-    res.statusCode = code;
-    res.statusMessage = code_msg;
-    res.setHeader('Content-Type','text/html');
-    res.write(page);
-    res.end();
-  });
+        //-- Generar la respusta en función de las variables
+        //-- code, code_msg y page
+        res.statusCode = code;
+        res.statusMessage = code_msg;
+        res.setHeader('Content-Type','text/html');
+        res.write(page);
+        res.end();
+    }
+});
   
-  //-- Activar el servidor: ¡Que empiece la fiesta!
-  server.listen(PUERTO);
-  
-  console.log("Server activado!. Escuchando en puerto: " + PUERTO);
+//-- Activar el servidor: ¡Que empiece la fiesta!
+server.listen(PUERTO);
+
+console.log("Server activado!. Escuchando en puerto: " + PUERTO);
