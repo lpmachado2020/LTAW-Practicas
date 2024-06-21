fa0fd2e14698c3ec81cbd8e73b2b2173196dae33
lpmachado2020 <lp.machado.2020@alumnos.urjc.es>
2024-06-21 17:29:04 +0200

    Sonido implementado correctamente

--- P3/public/cliente.js
+++ P3/public/cliente.js
@@ -1,27 +1,29 @@
 //-- Elementos del interfaz
 const display = document.getElementById("display");
 const msg_entry = document.getElementById("msg_entry");
+const sound_notification = document.getElementById("sound-notification");
 
 //-- Crear un websocket. Se establece la conexión con el servidor
 const socket = io();
 
 socket.on("connect", () => {
-  const username = localStorage.getItem('username');
-  if (username) {
-    socket.emit('setUsername', username);
-  }
+    const username = localStorage.getItem('username');
+    if (username) {
+      socket.emit('setUsername', username);
+    }
 });
 
 socket.on("message", (msg) => {
-  display.innerHTML += '<p style="color:blue">' + msg + '</p>';
+    display.innerHTML += '<p style="color:blue">' + msg + '</p>';
+    sound_notification.play();  // Reproducir el sonido cuando se recibe un mensaje
 });
 
 //-- Al apretar el botón se envía un mensaje al servidor
 msg_entry.onchange = () => {
-  if (msg_entry.value) {
-    socket.send(msg_entry.value);
-  }
-  
-  //-- Borrar el mensaje actual
-  msg_entry.value = "";
+    if (msg_entry.value) {
+      socket.send(msg_entry.value);
+    }
+    
+    //-- Borrar el mensaje actual
+    msg_entry.value = "";
 }
